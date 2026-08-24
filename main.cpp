b2858238520987231a490d3d052dbd7ed2bf2c6c
exyrux <exyrux@gmail.com>
2026-08-24 17:15:23 +0300

    a boolean sphere check

--- main.cpp
+++ main.cpp
@@ -15,6 +15,17 @@ void write_color(std::ostream& out, const vec3& pixel_color) {
     out << r << " " << g << " " << b << "\n";
 }
 
+
+bool hit_sphere(const vec3& center, double radius, const ray& r) {
+    const vec3 distance = r.origin - center;
+    const double a = r.direction.dot_self();
+    const double b = 2 * dot(r.direction, distance);
+    const double c = distance.dot_self() - radius*radius;
+    const double discr = b*b-4*a*c;
+    return discr>=0;
+}
+
+
 int main() {
     std::ofstream out("image.ppm");
     const int horizontal = 800;
@@ -25,11 +36,17 @@ int main() {
 
     for (int j = 0; j < vertical; j++) {
         for (int i = 0; i < horizontal; i++) {
-
+            vec3 pixel_color;
             const ray r = cam.project(i, j);
-            const vec3 dir = unit_vec3(r.direction);
-            const double a = 0.5*(dir.y + 1.0);
-            vec3 pixel_color = a*vec3(1.0, 0.0, 1.0);
+
+            if (hit_sphere(vec3(0,0,-1), .5, r)) {
+                pixel_color = vec3(1.0, 1.0, 1.0);    
+            } else {
+                const vec3 dir = unit_vec3(r.direction);
+                const double a = 0.5*(dir.y + 1.0);
+                pixel_color = a*vec3(1.0, 0.0, 1.0);    
+            }
+            
             write_color(out, pixel_color);
 
         }
