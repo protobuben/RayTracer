c2206975e28d5a5c7d5796f8b698432bea09cf85
exyrux <exyrux@gmail.com>
2026-08-24 17:36:38 +0300

    basic light

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 #include "vec3.h"
 #include "ray.h"
@@ -16,20 +17,23 @@ void write_color(std::ostream& out, const vec3& pixel_color) {
 }
 
 
-bool hit_sphere(const vec3& center, double radius, const ray& r) {
+double hit_sphere(const vec3& center, double radius, const ray& r) {
     const vec3 distance = r.origin - center;
     const double a = r.direction.dot_self();
-    const double b = 2 * dot(r.direction, distance);
+    const double b = 2.0 * dot(r.direction, distance);
     const double c = distance.dot_self() - radius*radius;
     const double discr = b*b-4*a*c;
-    return discr>=0;
+    return (-b - std::sqrt(discr))/(2*a);
 }
 
 
 int main() {
     std::ofstream out("image.ppm");
-    const int horizontal = 800;
-    const int vertical = 600;
+    const int horizontal = 320;
+    const int vertical = 240;
+    const vec3 light_direction = vec3(-1.0, -1.0, -.5);
+
+
     out << "P3\n" << horizontal << " " << vertical << "\n255\n";
 
     camera cam(horizontal, vertical);
@@ -39,12 +43,17 @@ int main() {
             vec3 pixel_color;
             const ray r = cam.project(i, j);
 
-            if (hit_sphere(vec3(0,0,-1), .5, r)) {
-                pixel_color = vec3(1.0, 1.0, 1.0);    
+            const vec3 sphere_center = vec3(0,0,-1);
+            const double t = hit_sphere(sphere_center, .5, r);
+
+            if (t > 0) {
+                const vec3 sphere_point = r.at(t);
+                const vec3 norm = unit_vec3(sphere_point-sphere_center);
+                const double brightness = std::max(dot(-light_direction, norm), 0.0);
+                pixel_color = vec3(1.0, 1.0, 1.0) * (.1 + .9*brightness);
+
             } else {
-                const vec3 dir = unit_vec3(r.direction);
-                const double a = 0.5*(dir.y + 1.0);
-                pixel_color = a*vec3(1.0, 0.0, 1.0);    
+                pixel_color = vec3(0.0, 0.0, 0.0);    
             }
             
             write_color(out, pixel_color);
